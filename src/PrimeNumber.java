e22c53ab319bd7af42ecfc3d6f9bf8447e76df8b
unique-Dimon <pivkooooo@mail.ru>
2020-07-28 16:54:02 +0300

    US0001:  - added git ignore file

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -6,25 +6,26 @@ public class PrimeNumber {
         System.out.print("Enter number: ");
         Scanner n = new Scanner(System.in);
         int nth = n.nextInt();
-        int num = 1;// будет нашим простым числом
-        int counter = 0;
+        int num = 2;// будет нашим простым числом
+        int counter = 1;
         int i;
-        if (nth < 1) {
-            System.out.println("This number is not exist");
-        } else {
-            while (counter < nth) {
-                num++;//начнем проверять с 2
-                for (i = 2; i <= num; i++) {
-                    if (num % i == 0) {
-                        break;
+        if (nth < 1) System.out.println("This number is not exist");
+        else {
+            if (nth == 1) System.out.println("Value of 1th simple number is: " + num);
+            else {
+                while (counter < nth) {
+                    num++;//начнем проверять с 3
+                    for (i = 3; i <= num; i += 2) {
+                        if (num % i == 0) {
+                            break;
+                        }
+                    }
+                    if (i == num) {
+                        counter++;//если число простое увеличиваем счетчик
                     }
                 }
-                if (i == num) {
-                    counter++;//если число простое увеличиваем счетчик
-                }
+                System.out.println("Value of " + counter + "th simple number is: " + num);
             }
-            System.out.println("Value of " + counter + "th simple number is: " + num);
         }
     }
-
 }
